94dab8e60edad804253fc6c15b609005420c8359
bobberr <izotopenko@yandex.ru>
2016-09-28 23:42:35 +0300

    Third commit

---
+++ js/js.js
@@ -0,0 +1,11 @@
+$('.slider-block').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  prevArrow: $('.button_previous'),
+  nextArrow: $('.button_next')
+});
+
